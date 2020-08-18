06dfe8eb0bb8c44ac6db0721ec36545e07fd823c
madhumithaa <madhumithaa1411@gmail.com>
2020-08-18 15:46:08 +0530

    taken out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
